51c881e194a18455034b845eaec74d86cdf902ad
vaibhav <vaibhavsachdeva2000@gmail.com>
2023-03-04 20:47:21 +0530

    Form component of main is created

---
+++ src/components/Main/Form/Form.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
+import { v4 as uuidv4 } from 'uuid';
+
+import { useSpeechContext } from '@speechly/react-client';
+
+import useStyles from './styles';
+
+function Form() {
+
+    const classes = useStyles();
+
+   return (
+       <Grid container spacing={2}>
+           <Grid item xs={12}>
+               <Typography align="center" variant="subtitle2" gutterBottom >
+                   ...
+               </Typography>
+           </Grid>
+           <Grid item xs={6}>
+               <FormControl fullWidth>
+                   <InputLabel>Type</InputLabel>
+                   <Select>
+                       <MenuItem value="Income">Income</MenuItem>
+                       <MenuItem value="Expense">Expense</MenuItem> 
+                   </Select>
+               </FormControl>
+           </Grid>
+           <Grid item xs={6}>
+               <FormControl fullWidth>
+                   <InputLabel>Category</InputLabel>
+                   <Select>
+                       <MenuItem value="business">Business</MenuItem>
+                       <MenuItem value="salary">Salary</MenuItem> 
+                   </Select>
+                </FormControl>
+           </Grid>
+
+            <Grid item xs={6}>
+                <TextField type="number" label="Amount"  fullWidth />
+           </Grid>
+            <Grid item xs={6}>
+                <TextField type="date" label="Date"  fullWidth />
+           </Grid>
+           <Button className={classes.button} variant="outlined" color="primary" fullWidth >Create</Button>
+      
+    </Grid>
+  )
+}
+
+export default Form
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,7 +3,7 @@ import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@mater
 import { useSpeechContext } from '@speechly/react-client';
 // import { ExpenseTrackerContext } from '../../context/context';
 import useStyles from './styles';
-// import Form from './Form/Form';
+import Form from './Form/Form';
 // import List from './List/List';
 // import InfoCard from '../InfoCard';
 
@@ -23,7 +23,7 @@ const Main = () => {
                   try saying income of 1 hunderd dollars in category salary for monday 
               </Typography>
               <Divider />
-              {/* {form} */}
+              <Form/>
           </CardContent>
           <CardContent className ={classes.cardContent}>
               <Grid container spacing={2}>
