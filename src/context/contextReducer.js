2b954b4961f4f4db615a00916aea61fd0f310a86
vaibhav <vaibhavsachdeva2000@gmail.com>
2023-03-05 10:14:56 +0530

    contextreducer doubt to be solved

---
+++ src/context/contextReducer.js
@@ -0,0 +1,22 @@
+const contextReducer = (state, action) =>
+{
+    let transactions;
+    switch (action.type)
+    {
+        case 'DELETE_TRANSACTION':
+             transactions = state.filter((t) => t.id !== action.payload);
+            return transactions;
+            
+        
+        case 'ADD_TRANSACTION':
+            transactions = [action.payload, ...state]
+            return transactions;
+        
+        default:
+            return state;
+    }
+}
+
+///here transaction and  state are same check after deploying
+
+export default contextReducer;
