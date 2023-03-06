9ba657fc1ebf6e2ac12d89e29c23edf3536d3436
vaibhav <vaibhavsachdeva2000@gmail.com>
2023-03-06 19:45:38 +0530

    speechly component added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { SpeechProvider } from '@speechly/react-client';
 
 import './index.css'
 import { Provider } from './context/context';
 
 
 ReactDOM.render(
+    <SpeechProvider appId = "61cb7584-2d15-4347-8ec3-c1440a7f3874"  language = "en-US">
     <Provider>
         <App />
-    </Provider>
+        </Provider>
+    </SpeechProvider>
     , document.getElementById('root'));
