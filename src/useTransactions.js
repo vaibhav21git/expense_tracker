9b0b9a158c9b0510214cf768d8c2b7bbc0cf00f9
vaibhav <vaibhavsachdeva2000@gmail.com>
2023-03-06 18:37:23 +0530

    use transactions updated

--- src/useTransactions.js
+++ src/useTransactions.js
@@ -1,37 +1,32 @@
 import { useContext } from 'react';
-import { ExpenseTrackerContext } from './context/context'
+import { ExpenseTrackerContext } from './context/context';
+
 import { incomeCategories, expenseCategories, resetCategories } from './constants/categories';
 
+const useTransactions = (title) => {
+  resetCategories();
+  const { transactions } = useContext(ExpenseTrackerContext);
+  const rightTransactions = transactions.filter((t) => t.type === title);
+  const total = rightTransactions.reduce((acc, currVal) => acc += currVal.amount, 0);
+  const categories = title === 'Income' ? incomeCategories : expenseCategories;
 
-const UseTransactions = (title) =>
-{
-    resetCategories();
+  rightTransactions.forEach((t) => {
+    const category = categories.find((c) => c.type === t.category);
 
-    const { transactions } = useContext(ExpenseTrackerContext);
+    if (category) category.amount += t.amount;
+  });
 
-    const transactionsPerType = transactions.filter((t) => t.type === title);
-    const total = transactionsPerType.reduce((acc, currVal) => acc += currVal.amount, 0);
-    const categories = title === 'Income' ? incomeCategories : expenseCategories;
+  const filteredCategories = categories.filter((sc) => sc.amount > 0);
 
-    transactionsPerType.forEach((t) => {
-        
-        const category = categories.find((c) => t.category = c.type)
-        
-        if (category) category.amount += t.amount;
-
-    });
-
-    const filteredCategories = categories.filter((c) => c.amount > 0);
-
-    const chartData = {
-         
+  const chartData = {
     datasets: [{
       data: filteredCategories.map((c) => c.amount),
       backgroundColor: filteredCategories.map((c) => c.color),
-        }],
-        
+    }],
     labels: filteredCategories.map((c) => c.type),
   };
 
   return { filteredCategories, total, chartData };
-}
+};
+
+export default useTransactions;
